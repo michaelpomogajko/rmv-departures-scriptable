12a1fc96a62fbd2f9d603bc3de23ca750967d52b
Michael Pomogajko <michael.pomogajko@pwc.com>
2022-01-24 11:04:57 +0100

    replacing strings

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ const getDepartures = async (stopId) => {
   }
 
   const sanitizeDir = (direction) => {
-    const replaced = direction.replace('Frankfurt (Main)', 'FFM');
+    const replaced = direction.replace('Frankfurt (Main)', 'FFM').replace('Hauptbahnhof', 'Hbf').replace('Bahnhof', 'Bf');
     const shortened = replaced.length >= MAX_CHAR_LENGTH ? replaced.substring(0, MAX_CHAR_LENGTH) + '...' : replaced;
     return shortened;
   }
